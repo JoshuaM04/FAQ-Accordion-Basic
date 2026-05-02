bd46937925054ab700c3afa5434a985ecbb989bd
Viper <pruncle21@gmail.com>
2026-05-02 12:33:34 -0500

    Updated script.js to make DOM queries for the drop-down sections and buttons utilizing node-list. A handler function ensures they share the same index, so the buttons are paired with their respective drop-down section. Utilized toggle attributes for the attributes such as hidden-aria and aria-expanded which will be further fine-tuned to ensure accessibility. Imported google fonts to apply styling to the text in accordance with the base design and adjusted the layout organization.

---
+++ script.js
@@ -0,0 +1,12 @@
+const buttons = document.querySelectorAll("button");
+const dropDowns = document.querySelectorAll(".drop-down");
+
+for (let i = 0; i < 4; i++) {
+    buttons[i].addEventListener('click', () => buttonHandler(i));
+}
+
+function buttonHandler(i) {
+    dropDowns[i].classList.toggle("open-section");
+    dropDowns[i].toggleAttribute("aria-hidden");
+    dropDowns[i].toggleAttribute("aria-expanded");
+}
